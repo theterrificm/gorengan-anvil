082df5e9b800ada8cfc46eb8b74642e1f6a181e8
musafhanif97@gmail.com <musafhanif97@gmail.com>
2025-03-28 22:20:27 +0000

    Created Main and Home; Edited Account, Contact, Header, Login and 2 more; Deleted Form1, HomePageMinimized, Homepage, MainNew and 1 more

--- client_code/Account/__init__.py
+++ client_code/Account/__init__.py
@@ -9,15 +9,3 @@ class Account(AccountTemplate):
 
     # Any code you write here will run before the form opens.
 
-  def contact_us_link_click(self, **event_args):
-    open_form('Contact')
-
-  def upgrade_page_link_click(self, **event_args):
-    open_form('Pricing')
-
-  def account_page_link_click(self, **event_args):
-    open_form('Account')
-
-  def login_page_link_click(self, **event_args):
-    open_form('Login')
-
--- client_code/Pricing/__init__.py
+++ client_code/Pricing/__init__.py
@@ -9,16 +9,3 @@ class Pricing(PricingTemplate):
 
     # Any code you write here will run before the form opens.
     
-  def contact_us_link_click(self, **event_args):
-    open_form('Contact')
-
-  def upgrade_page_link_click(self, **event_args):
-    open_form('Pricing')
-
-  def account_page_link_click(self, **event_args):
-    open_form('Account')
-
-  def login_page_link_click(self, **event_args):
-    open_form('Login')
-
-
